6247b023b753e2b1951af082f8e066ac2b7097b0
Andrew Stange <andrewstange@Andrews-Air-2.localdomain>
2021-08-02 22:52:56 -0400

    finish translation of ARM asm to GNU asm

--- src/kernel.s
+++ src/kernel.s
@@ -5,81 +5,70 @@
 //
 //  Conversion from Legacy ARM assembly (armasm) to GNU assembly syntax (armclang/as):
 //  https://developer.arm.com/documentation/dui0742/k/Migrating-from-armasm-to-the-armclang-Integrated-Assembler/Overview-of-differences-between-armasm-and-GNU-syntax-assembly-code?lang=en
-//
+//  GNU asm syntax: https://sourceware.org/binutils/docs/as/index.html#SEC_Contents
+//  ARM asm syntax: https://www.keil.com/support/man/docs/armasm/armasm_dom1361290000455.htm
 
 
-			//AREA |.text|,CODE,READONLY,ALIGN=2
+			//AREA |.text|,CODE,READONLY,ALIGN=2        /// TODO not sure this is correct translation
             .section .text,"x"
             .balign 4
-			//THUMB
             .thumb
             .syntax unified
-			//PRESERVE8
-            .eabi_attribute Tag_ABI_align_preserved, 1
+            .eabi_attribute Tag_ABI_align_preserved, 1      // PRESERVE8 in legacy ARM asm, tell assembler that we
+                                                            // preserve 8 byte stack alignment
+                                                            // https://developer.arm.com/documentation/100070/0608/gap1499870365595
 
-
-
-			// EXTERN currentPt                // actually &currentPt, not currentPt itself
-            // TODO EXTERN command is not addressed in migration, try .global or .extern for now??
-            .extern currentPt
-
-
-
-
-			//IMPORT priorityScheduler
-            .global priorityScheduler     // need a .type here??
+            .extern currentPt               // actually &currentPt, not currentPt itself
+            .global priorityScheduler       // this is a C function defined elsewhere, allow the linker to find it
 
 
 
 // handle PendSV interrupt.  Triggered by SysTick.  Used to enact a context switch between threads
-//EXPORT PendSV_Handler
 .global PendSV_Handler
-.type PendSV_Handler STT_FUNC // https://developer.arm.com/documentation/dui0742/k/Migrating-from-armasm-to-the-armclang-Integrated-Assembler/Miscellaneous-directives?lang=en
-PendSV_Handler:                      // hardware saves r0,r1,r2,r3,r12,lr,pc,psr just by calling this function
+.type PendSV_Handler STT_FUNC           // https://developer.arm.com/documentation/dui0742/k/Migrating-from-armasm-to-the-armclang-Integrated-Assembler/Miscellaneous-directives?lang=en
+PendSV_Handler:                         // hardware saves r0,r1,r2,r3,r12,lr,pc,psr just by calling this function
 	// save state of thread being suspended
-	CPSID	  I                     // disable interrupts
-	PUSH 	  {R4-R11}              // store R4-R11 on stack
-	LDR 	  R0,=currentPt         // R0 = &currentPt
-	LDR		  R1,[R0]               // R1 = currentPt
-	STR 	  SP,[R1]               // currentPt->stackPt = SP --> store current stack pointer in TCB
+	cpsid	  I                     // disable interrupts
+	push 	  {R4-R11}              // store R4-R11 on stack
+	ldr 	  R0,=currentPt         // R0 = &currentPt
+	ldr		  R1,[R0]               // R1 = currentPt
+	str 	  SP,[R1]               // currentPt->stackPt = SP --> store current stack pointer in TCB
 
     // determine which thread to run next
-    PUSH	  {R0,LR}
-	BL		  priorityScheduler   // call priorityScheduler --> select next thread to run
-	POP		  {R0,LR}
+    push	  {R0,LR}
+	bl		  priorityScheduler   // call priorityScheduler --> select next thread to run
+	pop		  {R0,LR}
 
 	// load TCB of next thread to run
-	LDR 	  R1,[R0]               // R1 = next thread to run (determined in C scheduler function)
-	LDR 	  SP,[R1]               // set stack pointer for next thread to run
-	POP		  {R4-R11}              // restore R4-R11 from that function's stack
-	CPSIE	  I                     // enable interrupts
-	BX		  LR                    // exit function
+	ldr 	  R1,[R0]               // R1 = next thread to run (determined in C scheduler function)
+	ldr 	  SP,[R1]               // set stack pointer for next thread to run
+	pop		  {R4-R11}              // restore R4-R11 from that function's stack
+	cpsie	  I                     // enable interrupts
+	bx		  LR                    // exit function
 // END PendSV_Handler
 
 
 
 
 // start the scheduler (initial context switch into first thread to run)
-//EXPORT schedulerLaunch
 .global schedulerLaunch
 .type schedulerLaunch STT_FUNC      // not clear if this should be .type or .text ...
 schedulerLaunch:
     // set stack pointer to value for first thread
-	LDR		R0,=currentPt           // R0 = &current_Pt
-	LDR		R2,[R0]                 // R2 = current_Pt
-	LDR		SP,[R2]                 // SP = current_Pt->stackPt
-	POP		{R4-R11}                // restore registers from this stack
-	POP		{R0-R3}
-	POP 	{R12}
+	ldr		R0,=currentPt           // R0 = &current_Pt
+	ldr		R2,[R0]                 // R2 = current_Pt
+	ldr		SP,[R2]                 // SP = current_Pt->stackPt
+	pop		{R4-R11}                // restore registers from this stack
+	pop		{R0-R3}
+	pop 	{R12}
 
-	ADD		SP,SP,#4                // skip over link register value stored on stack
-	POP		{LR}                    // load LR with PC value stored on stack (function pointer of first thread task)
-	ADD		SP,SP,#4                // skip PSR value on stack
-	CPSIE    I                      // enable interrupts
-	BX		 LR                     // return from subroutine
+	add		SP,SP,#4                // skip over link register value stored on stack
+	pop		{LR}                    // load LR with PC value stored on stack (function pointer of first thread task)
+	add		SP,SP,#4                // skip PSR value on stack
+	cpsie    I                      // enable interrupts
+	bx		 LR                     // return from subroutine
 // END schedulerLaunch
 
-    // ALIGN
-    .balign         // this might be invalid, not sure this carries over completely from ARM asm
-    // END
+    .balign 4           // in ARM asm, this was ALIGN with the AREA directive having an ALIGN expression value of 2,
+                        // causing this to be aligned on a 2^2 == 4 byte boundary
     .end
